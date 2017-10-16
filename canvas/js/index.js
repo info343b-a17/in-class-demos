e3f58e9f22b30552709bbac69a470fdadf599798
Joel Ross <joelross@uw.edu>
2017-10-16 08:54:00 -0700

    Add javascript starter

---
+++ canvas/js/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Get a reference to the drawing context. Do not modify! */
+const canvas = document.getElementById('canvas'); //reference the canvas element
+const brush = canvas.getContext('2d'); //the drawing context
+
+/* code goes here! */
+
---
+++ javascript/js/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+/* code goes here */
