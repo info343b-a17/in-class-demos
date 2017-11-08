e0940788eae022f7dddb3b5fc4f166c876681d8b
Joel Ross <joelross@uw.edu>
2017-11-08 11:40:07 -0800

    Added state and events

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -9,11 +9,28 @@ const SAMPLE_TASKS = [
 
 class App extends Component {
 
+  constructor(props){
+    super(props) //hey dad, set up this.props
+
+    //initial state values
+    this.state = {
+      currentTime: new Date(),
+      incompleteCount: 'A lot'
+    };
+
+    window.setInterval(() => {
+
+      let updatedState = {currentTime: new Date()};
+      this.setState(updatedState);
+
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="container">
-        <Clock />
-        <p className="lead">Things I have to do (2+)</p>
+        <Clock time={this.state.currentTime} />
+        <p className="lead">Things I have to do ({this.state.incompleteCount})</p>
         <TaskList />
         <AddTaskForm />
       </div>
@@ -24,17 +41,47 @@ class App extends Component {
 class Clock extends Component {
   render() {
     return (
-        <div className="font-italic">{"demo o'clock"}</div>
+        <div className="font-italic">{this.props.time.toLocaleTimeString()}</div>
     );  
   }
 }
 
 class TaskList extends Component {  
-  render() {   
+
+  constructor(props){
+    super(props);
+
+    //initial values
+    this.state = {
+      tasks: SAMPLE_TASKS    
+    }
+  }
+
+  toggleFinished(taskId){
+    let updatedTasks = this.state.tasks.map((task) => {
+      if(task.id === taskId){
+        task.complete = !task.complete; //toggle
+      }
+      return task;
+    })
+
+    this.setState({tasks:updatedTasks});
+  }
+
+  render() { 
+    
+    //[{} {} {}] ==> [<Task> <Task> <Task>]
+    let taskItemsArray = this.state.tasks.map((task) => {
+      return <Task 
+                key={task.id} 
+                task={task} 
+                toggleCallback={(taskId) => {this.toggleFinished(taskId)} }  
+                />;
+    })
+
     return (
       <ol>
-        <Task />
-        {/* tasks go here */}
+        {taskItemsArray}
       </ol>
     );
   }
@@ -42,11 +89,18 @@ class TaskList extends Component {
 
 class Task extends Component {
 
+  handleClick() {
+    console.log('You clicked on #', this.props.task.id);
+    this.props.toggleCallback(this.props.task.id);
+  }
+
   render() {
 
+    let className = this.props.task.complete ? 'font-strike' : '';
+
     return (
-      <li>
-        task description
+      <li className={className} onClick={() => {this.handleClick()} }>
+        {this.props.task.description}
       </li>
     );
   }
