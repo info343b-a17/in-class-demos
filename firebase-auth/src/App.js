d9abd0bd414a2728b5f3295bc01f6c53534ce7f2
Joel Ross <joelross@uw.edu>
2017-11-15 06:01:15 -0800

    Starter for firebase-auth lecture

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import firebase from 'firebase/app';
 
 class App extends Component {
   constructor(props){
@@ -12,77 +11,22 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    // Add a listener and callback for authentication events
-    //save the "unsubscribeFunction"
-    this.authUnSubFunc = firebase.auth().onAuthStateChanged(firebaseUser => {
-      if(firebaseUser) {
-        console.log('Auth state changed: logged in as', firebaseUser.email);
-        this.setState({user:firebaseUser});
-      }
-      else{
-        console.log('Auth state changed: logged out');
-        this.setState({user: null}); //null out the saved state
-      }
-    })
-  }
-
-  componentWillUnmount() {
-    //unsubscribe from auth state changes
-    this.authUnSubFunc();
-  }
-
   //A callback function for registering new users
   handleSignUp() {
     this.setState({errorMessage:null}); //clear old error
 
-    /* Create a new user and save their information */
-    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(firebaseUser => {
-        //include information (for app-level content)
-        let profilePromise = firebaseUser.updateProfile({
-          displayName: this.state.username,
-        }); //return promise for chaining
-
-        return profilePromise;
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState({errorMessage: err.message})
-      })
   }
 
   //A callback function for logging in existing users
   handleSignIn() {
     this.setState({errorMessage:null}); //clear old error
     
-    /* Sign in the user */    
-    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-      .catch((err) => {
-        console.log(err)
-        this.setState({errorMessage: err.message})
-      });
   }
 
   //A callback function for logging out the current user
   handleSignOut(){
     this.setState({errorMessage:null}); //clear old error
     
-    /* Sign out the user, and update the state */
-    firebase.auth().signOut()
-      .catch((err) => {
-        console.log(err)
-        this.setState({errorMessage: err.message})
-      });
-  }
-  
-  handleChange(event) {
-    let field = event.target.name; //which input
-    let value = event.target.value; //what value
-
-    let changes = {}; //object to hold changes
-    changes[field] = value; //change this field
-    this.setState(changes); //update state
   }
 
   render() {
@@ -92,9 +36,6 @@ class App extends Component {
           <p class="alert alert-danger">{this.state.errorMessage}</p>
         }
 
-        {this.state.user && 
-          <div className="alert alert-success"><h1>Logged in as {this.state.user.displayName}</h1></div>
-        }
 
         <div className="form-group">
           <label>Email:</label>
