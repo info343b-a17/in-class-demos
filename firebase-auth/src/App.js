b4d243f44ad56b97d6146b34793c90dfe3dcc7f0
Joel Ross <joelross@uw.edu>
2017-11-15 11:31:11 -0800

    End of lecture code

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import firebase from 'firebase/app';
 
 class App extends Component {
   constructor(props){
@@ -11,31 +12,75 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.unregisterFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      if(firebaseUser){ //someone logged in!
+        this.setState({user: firebaseUser});
+      }
+      else { //someone logged out
+        this.setState({user:null});
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unregisterFunction();
+  }
+
   //A callback function for registering new users
   handleSignUp() {
     this.setState({errorMessage:null}); //clear old error
 
+    firebase.auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((firebaseUser) => {
+          //do some stuff with the user
+          let promise = firebaseUser.updateProfile({displayName:this.state.username});
+          return promise;
+        })
+        .catch((err) => this.setState({errorMessage: err.message}))
+        .then(() => {
+          this.setState({email:'',password:'',username:''});          
+        })
   }
 
   //A callback function for logging in existing users
   handleSignIn() {
     this.setState({errorMessage:null}); //clear old error
-    
+
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .catch((err) => this.setState({errorMessage: err.message}))    
   }
 
   //A callback function for logging out the current user
   handleSignOut(){
     this.setState({errorMessage:null}); //clear old error
     
+    firebase.auth().signOut()
+      .catch((err) => this.setState({errorMessage: err.message}))    
+  }
+
+  handleChange(event) {
+    let field = event.target.name;
+    let value = event.target.value;
+
+    let changes = {};
+    changes[field] = value;
+    this.setState(changes);
   }
 
   render() {
     return (
       <div className="container">
         {this.state.errorMessage &&
-          <p class="alert alert-danger">{this.state.errorMessage}</p>
+          <p className="alert alert-danger">{this.state.errorMessage}</p>
         }
 
+        {this.state.user && 
+          <div className="alert alert-success">
+            <h1>Logged in as {this.state.user.displayName}</h1>
+          </div>
+        }
 
         <div className="form-group">
           <label>Email:</label>
--- firebase-auth/src/index.js
+++ firebase-auth/src/index.js
@@ -5,6 +5,18 @@ import 'bootstrap/dist/css/bootstrap.css'; //bootstrap (bundled)
 
 import App from './App';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD6cAxpDZt0orcc2YnQKhfSMH34f-dgbTA",
+    authDomain: "info343b-a17-demo.firebaseapp.com",
+    databaseURL: "https://info343b-a17-demo.firebaseio.com",
+    projectId: "info343b-a17-demo",
+    storageBucket: "info343b-a17-demo.appspot.com",
+    messagingSenderId: "828307407292"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
