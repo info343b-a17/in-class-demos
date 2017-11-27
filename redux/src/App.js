21e5c23c36819b689d466b200b68fad76b7fe417
Joel Ross <joelross@uw.edu>
2017-11-27 07:14:11 -0800

    Add initial redux demo app

---
+++ redux/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props) //hey dad, set up this.props
+
+    //initial state values
+    this.state = {
+      cookies: [],
+      baked: 0
+    };
+  }
+
+  addCookie(flavor){
+    let updatedCookies = this.state.cookies.concat({
+      cookieId: this.state.baked,
+      flavor:flavor
+    });
+    this.setState({
+      cookies:updatedCookies, 
+      baked:this.state.baked+1
+    });
+  }
+
+  eatCookie(cookieId) {
+    let updatedCookies = this.state.cookies.filter((c) => c.cookieId !== cookieId);
+    this.setState({cookies: updatedCookies});
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <CookieButton
+          flavor='ChocolateChip'
+          makeCookieCallback={(c) => this.addCookie(c)}
+          />
+        <CookieButton
+          flavor='Rainbow'
+          makeCookieCallback={(c) => this.addCookie(c)}
+          />
+        <CookieSheet 
+          cookies={this.state.cookies} 
+          eatCookieCallback={(c) => this.eatCookie(c)}
+          />
+      </div>
+    );
+  }
+}
+
+class CookieButton extends Component {
+  handleClick(){
+    this.props.makeCookieCallback(this.props.flavor);
+  }
+
+  render() {
+    return <button 
+      className="btn btn-secondary ml-2"
+      onClick={() => this.handleClick()} 
+      >
+      Make {this.props.flavor} Cookie
+      </button>
+  }
+}
+
+class CookieSheet extends Component {
+  render() {
+    let cookieImgs = this.props.cookies.map((c) => {
+      return <img 
+        key={c.cookieId} 
+        src={'img/'+c.flavor.toLowerCase()+'-cookie.png'} 
+        alt={c.flavor}
+        onClick={() => this.props.eatCookieCallback(c.cookieId)}
+        />
+    });
+
+    return (
+      <div className="my-3">{cookieImgs}</div>
+    )
+  }
+}
+
+export default App;
