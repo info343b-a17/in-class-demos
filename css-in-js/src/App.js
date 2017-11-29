ebc163f9d13ab807443d398b226f87a84ef13151
Joel Ross <joelross@uw.edu>
2017-11-29 09:52:17 -0800

    Add css-in-js demo starter

---
+++ css-in-js/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Aspernatur iure esse quas officia!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
