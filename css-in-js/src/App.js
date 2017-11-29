c469d59701d7efb2ae5124ae6f6dee6aade9fab9
Joel Ross <joelross@uw.edu>
2017-11-29 11:06:25 -0800

    End of lecture demo

--- css-in-js/src/App.js
+++ css-in-js/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  alert: {
+    color: 'tomato',
+    backgroundColor:'yellow',
+    ':hover': {
+      backgroundColor: 'tomato',
+      color: 'yellow'
+    }  
+  },
+  success: {
+    color: 'green'
+  }
+})
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Aspernatur iure esse quas officia!</p>
+        <p className={css(styles.alert, styles.success)}>Lorem ipsum dolor sit amet.</p>
+        <p className={css(styles.success)}>Aspernatur iure esse quas officia!</p>
       </div>
     )
   }
